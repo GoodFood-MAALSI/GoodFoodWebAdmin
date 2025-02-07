b4e0c5f1004c8c78244a811712bd6934c211fb4b
LuaScale <luascale@gmail.com>
2025-02-07 16:25:59 +0100

    feat(dashboard&login): ajout de la page de dashboard et de la page de login

---
+++ goodfoodwebadmin/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "../app/components/dashboard/dashboard";
+
+export default function Home() {
+  return (
+    <html>
+      <body>
+        <div>
+          <Dashboard/>
+        </div>
+      </body>
+    </html>
+  );
+}
